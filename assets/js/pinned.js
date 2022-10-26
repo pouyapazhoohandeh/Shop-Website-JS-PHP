aeff32d0ca662289957204de873c39f3de403e64
pouyapazhoohandeh <pouya201684pp@gmail.com>
2022-10-26 16:30:43 +0330

    add Some File and Change Index Page Style and Element

--- assets/js/pinned.js
+++
@@ -1,9 +0,0 @@
-/*
- * Skypack CDN - simplex-noise@4.0.1
- * Remove "min" from url for additional help & instructions.
- */
-
-export * from '/-/simplex-noise@v4.0.1-pbFxWb2JinfiImsyvo9X/dist=es2020,mode=imports,min/optimized/simplex-noise.js';
-export {default} from '/-/simplex-noise@v4.0.1-pbFxWb2JinfiImsyvo9X/dist=es2020,mode=imports,min/optimized/simplex-noise.js';
-
-// Perf Optimization: Preload Deep Imports
