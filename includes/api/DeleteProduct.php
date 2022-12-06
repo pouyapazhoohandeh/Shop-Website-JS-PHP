856fc069ff6c217a26f3db42998d701b3101e8cf
pouyapazhoohandeh <pouya201684pp@gmail.com>
2022-12-06 22:00:06 +0330

    add Del Product php file

---
+++ includes/api/DeleteProduct.php
@@ -0,0 +1,13 @@
+<?php
+ try{
+    require_once "../DataBase/database.php";
+    $ProductID = $_POST['ProductID'];
+    $sql="DELETE FROM product WHERE id='$ProductID'";
+    $conn=Connect();
+    $conn->exec($sql);
+}
+catch(PDOException $e){
+    echo "Error!". $e->getMessage(); 
+}
+
+?>
