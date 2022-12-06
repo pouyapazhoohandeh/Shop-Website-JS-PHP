aafcbb08f73512184ddeb40a44a2c599436eed37
pouyapazhoohandeh <pouya201684pp@gmail.com>
2022-12-06 21:09:22 +0330

    add loadProduct Function and Load a Latest Product

--- assets/js/GetProduct.js
+++ assets/js/GetProduct.js
@@ -1,2 +1,20 @@
 "use strict"
 
+function loadProduct() { 
+  $.ajax({
+    type : "get",
+    url : "includes/api/GetProduct.php",
+    async : "true",
+    success : function(Result){
+     
+      $('.PR-Table .ProductList').html(Result);
+    
+    }
+  });
+ }
+
+$(document).ready(function(){
+    
+   loadProduct();
+  
+  });
