bb94f9b848336ef3127f0bf511c0bdfeb2cad5c7
pouyapazhoohandeh <pouya201684pp@gmail.com>
2022-12-06 21:11:51 +0330

    using ajax and del product and load a product

--- assets/js/DeleteProduct.js
+++ assets/js/DeleteProduct.js
@@ -3,4 +3,18 @@ $('body').on('click','.delete',function () {
     var ProductID = $(this).closest("tr")   // Finds the closest row <tr> 
     .find(".id")     // Gets a descendent with class="nr"
     .text();
+    $.ajax({
+        type : "POST",
+        url : "includes/api/DelProduct.php",
+        data : {'ProductID':ProductID},
+        async : "true",
+        success : function(Result){
+            Swal.fire(
+                'محصول با موفقیت حذف شد.',
+                '',
+                'success'
+              )
+              loadProduct(); 
+        }
+      });
  });
