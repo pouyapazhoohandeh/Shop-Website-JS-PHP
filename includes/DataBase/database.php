962e47ce8b52575ae476c9464ef80a21f6815f23
pouyapazhoohandeh <pouya201684pp@gmail.com>
2022-12-06 20:50:56 +0330

    delete echo message from dataase php file

--- includes/DataBase/database.php
+++ includes/DataBase/database.php
@@ -13,7 +13,6 @@ function Connect(){
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       $conn->exec("SET NAMES utf8");
-      echo "Connected successfully";
       return $conn;
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
