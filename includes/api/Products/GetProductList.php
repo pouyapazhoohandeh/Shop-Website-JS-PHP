c9c738a3d344336c162a300549f8a45b8bf69b04
pouyapazhoohandeh <pouya201684pp@gmail.com>
2023-01-08 16:58:16 +0330

    Add Product List

--- includes/api/Products/GetProductList.php
+++ includes/api/Products/GetProductList.php
@@ -1,24 +1,12 @@
 <?PHP
 require_once "../../DataBase/database.php";
 $conn=Connect();
-$sth = $conn->prepare("SELECT * FROM product");
+$limit = 3;
+if (isset($_POST["page"])) { $page  = $_POST["page"]; } else { $page=1; };
+$start_from = ($page-1) * $limit;
+
+$sth = $conn->prepare("SELECT * FROM product limit $start_from,$limit");
 $sth->execute();
 $row = $sth->fetchAll(PDO::FETCH_ASSOC);
 $result=$row;
 echo json_encode($result);
-
-// foreach($result as $data){
-//     echo '<tr>
-//             <td class="id">'.$data['id'].'</td>                                       
-//             <td class="name">'.$data['Name'].'</td>                                    
-//             <td class="cat">'.$data['Category'].'</td>
-//             <td class="price">'.$data['Price'].'</td>
-//             <td class="quantity">'.$data['Quantity'].'</td>
-//             <td class="disc">'.$data['Discount'].'</td>
-//             <td class="desc">'.$data['Description'].'</td>
-//             <td class="td-btn">
-//                 <a href="#editEmployeeModal" class="edit btnupdate" data-toggle="modal"><i class="fa-regular fa-pen-to-square" data-toggle="tooltip" title="Edit"></i></a>
-//                 <a href="#deleteEmployeeModal"  class="delete" data-toggle="modal"><i class="fa-regular fa-trash-can" data-toggle="tooltip" title="Delete"></i></a>
-//             </td>
-//           </tr>';
-// }
